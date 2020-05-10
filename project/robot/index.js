13c5214c4c4032dd8941cbe6d17c4efe4c96fa4f
Santiago Guerra <guerrasantiago19@gmail.com>
2020-05-09 20:44:07 -0500

    Create a function to transform an array to a graph

--- project/robot/index.js
+++ project/robot/index.js
@@ -1,4 +1,4 @@
-const roads = [
+export const roads = [
   "Alice's House->Bob's House", "Alice's House->Cabin",
   "Alice's House->Post Office", "Bob's House->Town Hall",
   "Daria's House->Ernie's House", "Daria's House->Town Hall",
@@ -7,3 +7,24 @@ const roads = [
   'Marketplace->Post Office', 'Marketplace->Shop',
   'Marketplace->Town Hall', 'Shop->Town Hall',
 ];
+
+export const arrayToGraph = array => {
+  const graph = {};
+  const insertEdge = (from, to) => {
+    if (graph[from] === null || graph[from] === undefined) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  };
+
+  const eachRoad = array.map(road => road.split('->'));
+
+  eachRoad.forEach(elem => {
+    const [from, to] = elem;
+    insertEdge(from, to);
+    insertEdge(to, from);
+  });
+
+  return graph;
+};
